17b2ea34cf67354af97122ae88bc5b28a80d41a2
Dragon <115075047+Dragonbestgg1@users.noreply.github.com>
2025-01-29 13:58:36 +0200

    Update

---
+++ src/components/Land.tsx
@@ -0,0 +1,33 @@
+// src/components/land.tsx
+"use client";
+
+import { useState, useEffect } from "react";
+import styles from "../app/page.module.css";
+
+export default function Land() {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleActiveState = () => {
+    setIsActive(!isActive);
+  };
+
+  return (
+    <div className={styles.page}>
+      <header className={styles.header}>
+        <div className={styles.headerContent}>
+          <h1 className={styles.title}>VTDT Code Editor</h1>
+          <div className={styles.modalButton}>
+            <button
+              className={`${styles.button} ${isActive ? styles.active : ""}`}
+              onClick={toggleActiveState}
+            >
+              <span>Open Modal</span>
+            </button>
+          </div>
+        </div>
+      </header>
+      <main className={styles.main}></main>
+      <footer className={styles.footer}></footer>
+    </div>
+  );
+}
