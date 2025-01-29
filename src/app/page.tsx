36001907bca97e0f05a7262267265eb593efa4fa
Dragon <115075047+Dragonbestgg1@users.noreply.github.com>
2025-01-29 13:28:37 +0200

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,32 @@
+"use client";
+
+import { useState } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleActiveState = () => {
+    setIsActive(!isActive);
+  };
+
   return (
     <div className={styles.page}>
       <header className={styles.header}>
-        
+        <div className={styles.headerContent}>
+          <h1 className={styles.title}>VTDT Code Editor</h1>
+          <div className={styles.modalButton}>
+            <button
+              className={`${styles.button} ${isActive ? styles.active : ""}`}
+              onClick={toggleActiveState}
+            >
+              <span>Open Modal</span>
+            </button>
+          </div>
+        </div>
       </header>
-      <main className={styles.main}>
-      </main>
-      <footer className={styles.footer}>
-
-      </footer>
+      <main className={styles.main}></main>
+      <footer className={styles.footer}></footer>
     </div>
   );
 }
